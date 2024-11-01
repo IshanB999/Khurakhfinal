42e13d46381e6e92b0af02853a7733b1c6053ff5
anilwagle808 <anil.wagle808@gmail.com>
2024-11-01 13:22:33 +0545

    Create sign in page

--- mealplans/views.py
+++ mealplans/views.py
@@ -8,3 +8,6 @@ def index(request):
     template = loader.get_template('home.html')
 
     return HttpResponse(template.render())
+
+
+
---
+++ static/javascript/index.js
@@ -0,0 +1,7 @@
+function openDialog() {
+    document.getElementById('dialogOverlay').classList.add('show');
+}
+
+function closeDialog() {
+    document.getElementById('dialogOverlay').classList.remove('show');
+}
