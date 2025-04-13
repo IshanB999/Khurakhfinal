298a7c24b7d19a539597b78a5a10a9e40b08cf82
IshanB999 <ishanbartaula1223@gmail.com>
2025-04-13 14:42:56 +0545

    Pushing the final project

--- runserver.bat
+++ runserver.bat
@@ -3,7 +3,7 @@ REM Check if Python is in PATH or set its path manually
 SET PYTHON_PATH=python
 
 REM Run the Django server with network binding
-%PYTHON_PATH% manage.py runserver 0.0.0.0:8000
+%PYTHON_PATH% manage.py runserver localhost:8000
 
 REM Keep the terminal window open (optional)
 pause
